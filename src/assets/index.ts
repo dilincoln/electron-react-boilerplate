fc0abadead93a172ea5c1ae03e9e80f21690652f
Diego Lincoln <diego.lincoln@hotmail.com>
2021-11-21 20:02:17 -0300

    Arte Villa removida

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,25 +9,19 @@ import grpLit from './grpLit.mp4';
 import pluralBox1 from './pluralBox1.jpg';
 import pluralBox2 from './pluralBox2.jpg';
 import psg from './psg.png';
-import villaArmazem1 from './villaArmazem1.jpg';
-import villaArmazem2 from './villaArmazem2.jpg';
-import villaArmazem3 from './villaArmazem3.jpg';
 
 const assets = [
   { media: aeroPark, type: 'image', length: 8000 },
   { media: arenaEvo, type: 'image', length: 8000 },
   { media: belaDama, type: 'image', length: 8000 },
-  { media: villaArmazem1, type: 'image', length: 8000 },
   { media: bistro1, type: 'image', length: 8000 },
   { media: bistro2, type: 'image', length: 8000 },
   { media: fiosDeAnjo, type: 'image', length: 8000 },
   { media: futeVolei, type: 'image', length: 8000 },
-  { media: villaArmazem2, type: 'image', length: 8000 },
   { media: grpLit, type: 'video', length: 15000 },
   { media: pluralBox1, type: 'image', length: 8000 },
   { media: pluralBox2, type: 'image', length: 8000 },
   { media: psg, type: 'image', length: 8000 },
-  { media: villaArmazem3, type: 'image', length: 8000 },
 ];
 
 export default assets;
