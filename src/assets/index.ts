c6ab5cb6730efbfef06ab7914c8571f337cba2e0
Diego Lincoln <diego.lincoln@hotmail.com>
2021-12-10 06:06:37 -0300

    Removido GRP LIT

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,37 +1,26 @@
 import aeroPark from './aero_park.png';
 import arenaEvo from './arena_evo.png';
 import belaDama from './belaDama.png';
-import bistro1 from './bistro1.png';
-import bistro2 from './bistro2.jpg';
-import fiosDeAnjo from './fiosDeAnjo.png';
+import dayse from './dayse.mp4';
+import espacoAero from './espacoAero.jpg';
+import espacoAero2 from './espacoAero2.jpg';
 import futeVolei from './futeVolei.png';
-import grpLit from './grpLit.mp4';
 import pluralBox1 from './pluralBox1.jpg';
 import pluralBox2 from './pluralBox2.jpg';
 import psg from './psg.png';
-import villa1 from './villa1.jpg';
-import villa2 from './villa2.jpg';
-import villa3 from './villa3.jpg';
-import villa4 from './villa4.jpg';
-import dayse from './dayse.mp4';
 
 const assets = [
   { media: aeroPark, type: 'image', length: 8000 },
-  { media: villa1, type: 'image', length: 8000 },
+  { media: espacoAero, type: 'image', length: 8000 },
   { media: belaDama, type: 'image', length: 8000 },
-  { media: bistro1, type: 'image', length: 8000 },
-  { media: villa2, type: 'image', length: 8000 },
-  { media: fiosDeAnjo, type: 'image', length: 8000 },
   { media: arenaEvo, type: 'image', length: 8000 },
   { media: futeVolei, type: 'image', length: 8000 },
   { media: dayse, type: 'video', length: 8000 },
   { media: pluralBox2, type: 'image', length: 8000 },
-  { media: bistro2, type: 'image', length: 8000 },
-  { media: grpLit, type: 'video', length: 15000 },
-  { media: villa3, type: 'image', length: 8000 },
+  { media: espacoAero2, type: 'image', length: 8000 },
+  // { media: grpLit, type: 'video', length: 15000 },
   { media: pluralBox1, type: 'image', length: 8000 },
   { media: psg, type: 'image', length: 8000 },
-  { media: villa4, type: 'image', length: 8000 },
 ];
 
 export default assets;
